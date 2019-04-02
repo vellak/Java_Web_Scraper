dd8cc7653ecf79c3d2bca88bb044330b25f70fed
Keith Vella <36503340+vellak@users.noreply.github.com>
2019-04-02 05:41:43 +0200

    Created scraper file,
    added actionListeners to buttons

---
+++ JavaWebScraper/src/Scraper.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+class Scraper {
+
+    public List<WebsiteDetails> listOfWebsites;
+    public String websitesVisited;
+
+    public static void Scrape(String text, String depthOfSearchText){
+
+    }
+
+}
